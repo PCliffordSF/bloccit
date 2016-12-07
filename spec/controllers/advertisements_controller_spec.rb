35faf6492c0f6b51f119a5097c0c93be8bbad569
Paul Clifford <paulclifford2570@gmail.com>
2016-12-07 00:17:11 +0000

    finish assignment 19

--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -17,23 +17,45 @@ RSpec.describe AdvertisementsController, type: :controller do
   end
 
 
+   describe "GET show" do
+     it "returns http success" do
+ 
+       get :show, {id: my_advertisement.id}
+       expect(response).to have_http_status(:success)
+     end
+     it "renders the #show view" do
 
+       get :show, {id: my_advertisement.id}
+       expect(response).to render_template :show
+     end
+ 
+     it "assigns my_advertisement to @advertisement" do
+       get :show, {id: my_advertisement.id}
 
+       expect(assigns(:advertisement)).to eq(my_advertisement)
+     end
+   end
+   
+   
 
-
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+    describe "GET new" do
+      it "returns http success" do
+        get :new
+        expect(response).to have_http_status(:success)
+      end
+ 
+ # #2
+      it "renders the #new view" do
+        get :new
+        expect(response).to render_template :new
+      end
+ 
+ # #3
+      it "instantiates @post" do
+        get :new
+        expect(assigns(:advertisement)).not_to be_nil
+      end
+    end
 
   # describe "GET #create" do
   #   it "returns http success" do
