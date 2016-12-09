0c4db02808ec9b1d85b12f7fa4de0f404f548d08
Paul Clifford <paulclifford2570@gmail.com>
2016-12-09 13:58:54 +0000

    finish assignment 20

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,27 +9,39 @@
 
  require 'random_data'
 
- # Create Posts
+ # # Create Posts
+ # 50.times do
+ # # #1
+ #   Post.create!(
+ # # #2
+ #     title:  RandomData.random_sentence,
+ #     body:   RandomData.random_paragraph
+ #   )
+ # end
+ # posts = Post.all
+ 
+ # # Create Comments
+ # # #3
+ # 100.times do
+ #   Comment.create!(
+ # # #4
+ #     post: posts.sample,
+ #     body: RandomData.random_paragraph
+ #   )
+ # end
+ 
+ # puts "Seed finished"
+ # puts "#{Post.count} posts created"
+ # puts "#{Comment.count} comments created"
+ 
+  # Create Posts
  50.times do
  # #1
-   Post.create!(
+   Question.create!(
  # #2
      title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
+     body:   RandomData.random_paragraph,
+     resolved: rand(1) == 1 ? true : false 
    )
  end
- posts = Post.all
- 
- # Create Comments
- # #3
- 100.times do
-   Comment.create!(
- # #4
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
- 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+ questions = Question.all
