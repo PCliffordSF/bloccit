23d6452a74afa95285fc85e54b8a2a93acba3f18
Paul Clifford <paulclifford2570@gmail.com>
2016-12-12 15:00:07 +0000

    finish checkpoint 23

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
+ 
+
 
 RSpec.describe Post, type: :model do
     
@@ -12,6 +14,13 @@ RSpec.describe Post, type: :model do
    let(:post) { topic.posts.create!(title: title, body: body) }
  
    it { is_expected.to belong_to(:topic) }
+   
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+   it { is_expected.to validate_presence_of(:topic) }
+ 
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
    describe "attributes" do
      it "has title and body attributes" do
