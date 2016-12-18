15990b023b363e409601ce8837c58a2d779a1c32
Paul Clifford <paulclifford2570@gmail.com>
2016-12-18 16:58:55 +0000

    finish checkpoint 28

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,9 +54,18 @@
    )
  end
  
- user = User.first
- user.update_attributes!(
-   email: 'paulclifford2570@gmail.com', # replace this with your personal email
+ # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
  
