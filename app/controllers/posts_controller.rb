02de98265d72a72acf26aab668d167d4eefab668
Paul Clifford <paulclifford2570@gmail.com>
2016-12-26 13:55:19 +0000

    finish assignment 30

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -73,5 +73,5 @@ class PostsController < ApplicationController
        redirect_to [post.topic, post]
      end
    end
-  
+   
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -23,4 +23,6 @@ class VotesController < ApplicationController
        @vote = current_user.votes.create(value: new_value, post: @post)
      end
    end
+   
+
 end
